9b6463b525751a86f5c17470f0576d3986bbc586
unknown <matheus-lb@hotmail.com>
2022-03-07 19:04:51 -0300

    exercicio 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -131,7 +131,7 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   return "Venha assistir ao filme " +filme.nome+ ", de " +filme.ano+ ", dirigido por "+filme.diretor+ " e estrelado por "+filme.atores+ "."
 }
 
 // EXERCÍCIO 12
