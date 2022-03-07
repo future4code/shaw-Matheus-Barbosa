d3ed70a751a5b210f4a1ae07d314eb02ed04592a
unknown <matheus-lb@hotmail.com>
2022-03-07 18:35:59 -0300

    exercicio 9

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -103,11 +103,20 @@ function retornaNPrimeirosPares(n) {
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
 
+    if (ladoA === ladoB && ladoB === ladoC){
+        return "Equilátero"
+    }
+    if (ladoA !== ladoB && ladoB !== ladoC && ladoA !== ladoC){
+        return "Escaleno"
+    }else{
+        return "Isósceles"
+    }
 }
+
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
