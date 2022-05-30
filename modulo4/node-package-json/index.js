f9e345591813141778628bd5deb1c71b94e4c359
unknown <matheus-lb@hotmail.com>
2022-05-30 20:27:43 -0300

    package-json

---
+++ modulo4/node-package-json/index.js
@@ -0,0 +1,51 @@
+// A) acessamos buscando o nome do arquivo pela linha de comando com o comando node
+
+// B)
+
+// let nome = "Matheus"
+// let idade = "31"
+// let futuro = Number(idade) + 7
+
+// console.log("Olá " + nome + "! Você tem " + idade + " anos. Em sete anos você terá " + futuro)
+
+
+// Exercicio 2
+
+// let operacao = process.argv[2]
+
+// let numero1 = Number(process.argv[3])
+
+// let numero2 = Number(process.argv[4])
+
+// switch(operacao){
+//     case "add" :
+//         console.log(`resultado : ${numero1+numero2}`)
+//         break;
+//     case "sub" :
+//         console.log(`resultado : ${numero1-numero2}`)
+//         break;
+
+//     case "mult" :
+//         console.log(`resultado : ${numero1*numero2}`)
+//         break;
+
+//     case "div" :
+//         console.log(`resultado : ${numero1/numero2}`)
+//         break;
+//     default:
+//         console.log("operação não encontrada")
+// }
+
+// Exercicio 3
+
+// let listaDeTarefas = ["acordar","trabalhar"]
+
+// listaDeTarefas = [...listaDeTarefas,process.argv[2]]
+
+// console.log(listaDeTarefas)
+
+
+
+
+
+
